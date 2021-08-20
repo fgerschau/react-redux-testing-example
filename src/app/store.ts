377f1e74f875b2d72eb993a65be04748040524d3
Felix Gerschau <felix.gerschau@gmail.com>
2021-08-20 14:43:14 +0200

    Add counter tests

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 
-export const store = configureStore({
+export const createStore = () => configureStore({
   reducer: {
     counter: counterReducer,
   },
 });
+
+export const store = createStore();
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
